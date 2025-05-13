37e6b063ceb7da0693037e66464390fffbace345
Ayuma Kaminosono <develop@ayumax.net>
2025-05-13 21:45:44 +0900

    Added froola files (#22)

---
+++ Froola/run_create_packages.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+Froola.exe plugin -n WindowCapture2D -p WindowCapture2DTest -v UE_5_5 -u git@github.com:ayumax/WindowCapture2D.git -b UE5.5 -t -c -o packages
+Froola.exe plugin -n WindowCapture2D -p WindowCapture2DTest -v UE_5_4 -u git@github.com:ayumax/WindowCapture2D.git -b UE5.4 -t -c -o packages
+Froola.exe plugin -n WindowCapture2D -p WindowCapture2DTest -v UE_5_3 -u git@github.com:ayumax/WindowCapture2D.git -b UE5.3 -t -c -o packages
