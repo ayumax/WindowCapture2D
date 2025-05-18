56de859fc0bae2e7654ac3d530427f873a52bb30
Ayuma Kaminosono <develop@ayumax.net>
2025-05-18 19:25:07 +0900

    Update version to 2.0.3 (#29)

--- Froola/run_tests_win.bat
+++ Froola/run_tests_win.bat
@@ -1,2 +1,2 @@
 cd /d %~dp0
-Froola.exe plugin -n WindowCapture2D -p WindowCapture2DTest -l ..\ -t -o results -e Windows
+Froola.exe plugin -n WindowCapture2D -p WindowCapture2DTest -v 5.3,5.4,5.5 -l ..\ -t -o results
