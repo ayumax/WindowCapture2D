d2d033d2904ffa68e1741b27f630d96d7cb214c3
Ayuma Kaminosono <develop@ayumax.net>
2025-05-18 19:25:07 +0900

    Update version to 2.0.3 (#29)

--- Froola/run_create_packages.bat
+++ Froola/run_create_packages.bat
@@ -1,4 +1,2 @@
 cd /d %~dp0
-Froola.exe plugin -n WindowCapture2D -p WindowCapture2DTest -v UE_5_5 -u git@github.com:ayumax/WindowCapture2D.git -b UE5.5 -t -c -o packages
-Froola.exe plugin -n WindowCapture2D -p WindowCapture2DTest -v UE_5_4 -u git@github.com:ayumax/WindowCapture2D.git -b UE5.4 -t -c -o packages
-Froola.exe plugin -n WindowCapture2D -p WindowCapture2DTest -v UE_5_3 -u git@github.com:ayumax/WindowCapture2D.git -b UE5.3 -t -c -o packages
+Froola.exe plugin -n WindowCapture2D -p WindowCapture2DTest -v 5.3,5.4,5.5 -u git@github.com:ayumax/WindowCapture2D.git -t -c -o packages
